341187c4650305c740d0f8ec6e65b8beff75246a
SarAvi21805 <alejandraviles2005@gmail.com>
2026-05-29 14:51:53 -0600

    fix: agregar propiedad snakeColor al componente Snake para personalizar el color de la serpiente

--- src/components/Snake.jsx
+++ src/components/Snake.jsx
@@ -1,10 +1,11 @@
-const Snake = ({ snakeSegments }) => {
+const Snake = ({ snakeSegments, snakeColor }) => {
     return(
         <>
             {snakeSegments.map((dot, i) => {
                 const style = {
                     left: `${dot[0]}%`,
                     top: `${dot[1]}%`,
+                    backgroundColor: snakeColor,
                 };
 
                 return <div key={i} className="snake-segment" style={style}></div>
