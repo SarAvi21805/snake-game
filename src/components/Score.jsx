650b9865b02f3a2320b5e1aadfef4128f5d89856
SarAvi21805 <alejandraviles2005@gmail.com>
2026-05-29 14:51:44 -0600

    fix: agregar bestScore al componente Score para mostrar el récord

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,5 +1,10 @@
-const Score = ({ score }) => {
-    return <h2>Puntaje: {score}</h2>
+const Score = ({ score, bestScore }) => {
+    return (
+        <div className="score-board">
+            <div><h2>Puntaje: {score}</h2></div>
+            <div><h2>Récord: {bestScore}</h2></div>
+        </div>
+    );
 };
 
 export default Score;
