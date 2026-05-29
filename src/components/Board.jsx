6f6203f9974efed7bbd174c00aa5122eb10dc626
SarAvi21805 <alejandraviles2005@gmail.com>
2026-05-29 14:51:29 -0600

    fix: agregar componente Walls y ajustar props en Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,8 @@
 import Snake from './Snake';
 import Food from './Food';
+import Walls from './Walls';
 
-const Board = ({ snake, food}) => {
+const Board = ({ snake, food, walls, snakeColor }) => {
     const style = {
         width: '400px',
         height: '400px',
@@ -9,8 +10,9 @@ const Board = ({ snake, food}) => {
 
     return (
         <div className='board' style={style}>
-            <Snake snakeSegments={snake} />
-            <Food dot={food}/>
+            <Snake snakeSegments={snake} snakeColor={snakeColor} />
+            <Food food={food} />
+            <Walls walls={walls} />
         </div>
     );
 };
