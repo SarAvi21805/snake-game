adac6386a4183b7f9fce0674a8b27a657895c20a
SarAvi21805 <alejandraviles2005@gmail.com>
2026-05-29 20:55:41 -0600

    feat: agregar soporte para túneles, bonificaciones y enemigos en el componente Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,11 @@
 import Snake from './Snake';
 import Food from './Food';
 import Walls from './Walls';
+import Tunnel from './Tunnel';
+import Bonus from './Bonus';
+import Enemy from './Enemy';
 
-const Board = ({ snake, food, walls, snakeColor }) => {
+const Board = ({ snake, food, walls, tunnels, snakeColor, bonus, enemy }) => {
     const style = {
         width: '400px',
         height: '400px',
@@ -13,6 +16,9 @@ const Board = ({ snake, food, walls, snakeColor }) => {
             <Snake snakeSegments={snake} snakeColor={snakeColor} />
             <Food food={food} />
             <Walls walls={walls} />
+            <Tunnel tunnels={tunnels} />
+            {bonus && <Bonus bonus={bonus} />}
+            <Enemy enemy={enemy} />
         </div>
     );
 };
