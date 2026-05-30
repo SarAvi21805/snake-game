a98a299220bd458a289e5305bcfb1216fa8dc436
SarAvi21805 <alejandraviles2005@gmail.com>
2026-05-29 20:56:04 -0600

    feat: agregar información del jugador y nivel al componente Score

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,17 @@
-const Score = ({ score, bestScore }) => {
+const Score = ({ playerName, score, bestScore, level }) => {
     return (
         <div className="score-board">
-            <div><h2>Puntaje: {score}</h2></div>
-            <div><h2>Récord: {bestScore}</h2></div>
+            <div>
+                <h3>{playerName}</h3>
+                <h2>Puntaje: {score}</h2>
+            </div>
+            <div>
+                <h2>Récord: {bestScore.score}</h2>
+                <p>{bestScore.name}</p>
+            </div>
+            <div>
+                <h2>Nivel: {level}</h2>
+            </div>
         </div>
     );
 };
